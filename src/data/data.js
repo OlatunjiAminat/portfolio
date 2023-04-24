3529f14d05f68e47ced99c34c0e15779292e40b7
Aminat Olatunji <olatunjiaminat1990@gmail.com>
2023-04-23 17:12:05 -0700

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+import RealEst from '../assets/projects/realestate.jpg'
+import GogeAfrica from '../assets/projects/goge-africa.jpeg'
+import FurnitureApp from '../assets/projects/furniture-app.jpeg'
+import Dashboard from '../assets/projects/dashboard.jpeg'
+import AdminDashboard from '../assets/projects/admin-dashboard.jpeg'
+
+
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:GogeAfrica,
+        github:"https://github.com/olatunde1998/portfolio-react",
+        live:"https://goge-africaa.netlify.app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Dashboard,
+        github:"https://github.com/olatunde1998/lendsqr-fe-test",
+        live:"https://rasheed-olatunde-lendsqr-fe-test.netlify.app/",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:FurnitureApp,
+        github:"https://github.com/olatunde1998/furniture-app",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:5,
+        name:"Next JS Application",
+        image: AdminDashboard,
+        github:"https://github.com/olatunde1998/admin-dashboard",
+        live:"https://geodev-admin-dashboard.netlify.app/",
+    },
+
+
+]
